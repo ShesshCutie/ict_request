fc25f35021bf04259a74e2013f80e2eb8fa9c3e7
Aldrin Delicias <202180165@psu.palawan.edu.ph>
2025-02-06 09:16:08 +0800

    status

--- feedback/resources/views/service_status.blade.php
+++ feedback/resources/views/service_status.blade.php
@@ -1,8 +1,7 @@
-
 @section('content')
 <div class="container">
-    <h1 class="text-center">Service Report</h1>
-    <p>This is the Service Report section.</p>
+    <h1 class="text-center text-dark">Service Report</h1>
+    <p class="text-dark">This is the Service Report section.</p>
     <!-- You can add more content here based on your requirements -->
 </div>
 @endsection
